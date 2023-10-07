b302dda058ac706c4c4772c387a5355ae0775345
Cesar Ramirez <cesaralejandroramirezzuluaga@gmail.com>
2023-10-07 09:26:59 -0500

    se agrega vesion simple del proyecto

---
+++ ReactToDoList/proyects/ToDoList/src/app.jsx
@@ -0,0 +1,23 @@
+import './app.css'
+import { List } from './list.jsx'
+ export function App (){
+    return(
+        <article className="tdl-article">
+            <div className='tdl-article-div'>
+            <h1 className="tdl-article-h1">Todo List +</h1>
+                <header className="tdl-article-header"> 
+                    <input className="tdl-article-header-input" type="text" placeholder="Add New Todo" />
+                    <div className="tdl-article-header-div" >
+                        <button className="tdl-article-header-div-button">Add</button>
+                    </div>
+                </header>
+            </div>
+            <section>
+               <List/>
+               <List/>
+               <List/>
+            </section>
+        </article>
+    )
+
+}
